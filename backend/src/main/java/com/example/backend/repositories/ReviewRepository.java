055cec7c53ca221aed0bb166138bb93027cb1caf
AnaPaulaL27 <apl.moreno@hotmail.com>
2022-06-28 16:46:04 +0100

    Completed Review Controller and Repository

---
+++ backend/src/main/java/com/example/backend/repositories/ReviewRepository.java
@@ -0,0 +1,26 @@
+package com.example.backend.repositories;
+
+import com.example.backend.models.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    //DERIVED QUERIES
+
+    List<Review> findReviewByTitleContainingIgnoreCase(String title);
+
+    List<Review> findReviewByDescriptionContainingIgnoreCase (String description);
+
+    List<Review> findReviewByDatePosted(LocalDate datePosted);
+
+    List<Review> findReviewByRating(double rating);
+
+    List<Review> findReviewByRatingGreaterThan(double rating);
+
+    List<Review> findReviewByIsVerifiedPurchase(boolean isVerifiedPurchase);
+
+}
