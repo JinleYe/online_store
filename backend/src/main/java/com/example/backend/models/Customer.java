1be9c214db05740989628396096d4114961eadfa
Iman Chaudhry <i-man-735@hotmail.com>
2022-06-28 12:25:27 +0100

    created Customer class with no annotations

---
+++ backend/src/main/java/com/example/backend/models/Customer.java
@@ -0,0 +1,87 @@
+package com.example.backend.models;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+    private User user;
+
+    private String name;
+
+    private String address;
+
+    private List<Order> orders;
+
+    private List<Review> reviews;
+
+
+    // Empty Constructor
+
+    private Customer(){}
+
+    //Constructor
+    public Customer(User user, String name, String address) {
+        this.user = user;
+        this.name = name;
+        this.address = address;
+        this.orders = new ArrayList<>();
+        this.reviews = new ArrayList<>();
+    }
+
+    //Getters and Setters
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "user=" + user +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", orders=" + orders +
+                ", reviews=" + reviews +
+                '}';
+    }
+}
+
+
