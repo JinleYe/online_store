d8c9950c25a6ce99b548ebc40f072be516b79477
adanabdillahi <adanabdillahi30@gmail.com>
2022-07-01 16:40:43 +0100

    home carousel

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -11,23 +11,56 @@ import xboxcontroller from "../images/xboxcontroller.png";
 import vrheadset from "../images/vrheadset.png";
 import joystick from "../images/joystick.png";
 import ps5controller from "../images/ps5controller.png";
+import leftArrow from "../images/left-arrow.png";
+import rightArrow from "../images/right-arrow.png";
 import Tile from "../components/Tile";
 import { useState } from "react";
 import SubTitle from "../components/SubTitle";
 
 const Home = () => {
   const [imageURL, setImageURL] = useState(headset);
+  const [counter, setCounter] = useState(0);
 
+  const handleIncrement = () => {
+    if (counter === featuredProducts.length - 1) {
+      setCounter(0);
+    } else {
+      setCounter(counter + 1);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (counter === 0) {
+      setCounter(featuredProducts.length - 1);
+    } else {
+      setCounter(counter - 1);
+    }
+  };
+
+  const featuredProducts = [headset, desktop, laptop];
 
   // setImage1(headset)
   return (
     <div className="outer-featured-container">
       <h1 className="featured-product-title1"> Featured Products</h1>
       <div className="tile-container">
+        <img
+          src={leftArrow}
+          alt="left arrow"
+          onClick={handleDecrement}
+          className="carousel__arrow carousel__arrow--left"
+        />
+
         {/* {setImageURL(headset)} */}
-        <Tile imageURL={headset} title="Headsets" />
-        <Tile imageURL={desktop} title="Desktops" />
-        <Tile imageURL={laptop} title="Laptops" />
+        <Tile imageURL={featuredProducts[counter]} title="Headsets" />
+        <Tile imageURL={featuredProducts[counter + 1]} title="Desktops" />
+        <Tile imageURL={featuredProducts[counter + 2]} title="Laptops" />
+        <img
+          src={rightArrow}
+          alt="right arrow"
+          onClick={handleIncrement}
+          className="carousel__arrow carousel__arrow--right"
+        />
       </div>
       <br />
       <br />
