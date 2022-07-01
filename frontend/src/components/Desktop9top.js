2a5c31cdbd3a08109a11a917c4021568fe109b46
AnaPaulaL27 <apl.moreno@hotmail.com>
2022-07-01 12:48:33 +0100

    Basic grid complete

--- frontend/src/components/Desktop9top.js
+++ frontend/src/components/Desktop9top.js
@@ -8,40 +8,64 @@ function Desktop9top() {
     <div>
         <div class="contact-grid">
 
-        <div class="contactleft-column">
+        <div class="contactleft-column2">
             <h1 className='contactcolumnheaderfont1'>Information About us</h1>
-            <br/>
-            <p className='contactcolumnfont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis neque ultrices mattis aliquam, malesuada diam est. Malesuada sem tristique amet erat vitae eget dolor lobortis. Accumsan faucibus vitae lobortis quis bibendum quam.
+            <p className='contactcolumnfont2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis neque ultrices mattis aliquam, malesuada diam est. Malesuada sem tristique amet erat vitae eget dolor lobortis. Accumsan faucibus vitae lobortis quis bibendum quam.
             </p>
         </div>
 
-        <div class="contactleft-column">
+
+        <div class="contactleft-column2">
             <h1 className='contactcolumnheaderfont1'>Contact Way</h1>
-            <br/>
-            <p className='contactcolumnfont'>
-            <ul id="list1">
-                    <li> Tel: 020 7462 7459 </li>
-                    <li> E-Mail:  </li>
-                    
-            </ul>
 
-            <ul id="list2">
-                    <li> MENU </li>
-                    <li> MENU </li>
-                    
-            </ul>
-                
-            </p>
-        </div>
+            <p className='contactcolumnfont2'>
+                <div class="row">
+                <div class="column">
+                    {/* <h2>Column 1</h2> */}
+                    <ul id="list1">
+                        <div id="leftbox1">
+                        <li> Tel: 020 7462 7459 </li>
+                        <li> E-Mail: Pixelatedparadise@store.com </li>
+                        </div>
+                    </ul>
+                </div>
 
+                <div class="column">
+                    {/* <h2>Column 2</h2> */}
+                    <ul id="list1">
+                        <div id="leftbox1">
+                        <li> Support Forum </li>
+                        <li> For over 24hr </li>  
+                        </div>
+                    </ul>
+                </div>
+                </div>
 
+                <div class="row">
+                <div class="column">
+                    {/* <h2>Column 3</h2> */}
+                    <ul id="list1">
+                        <div id="leftbox1">
+                        <li> London, England</li>
+                        <li> United Kingdom </li>
+                        </div>
+                    </ul>
+                </div>
 
-
-
-
-</div>
-
+                <div class="column">
+                    {/* <h2>Column 4</h2> */}
+                     <ul id="list1">
+                        <div id="leftbox1">
+                        <li> Free standard  shipping </li>
+                        <li> on all orders </li>  
+                        </div>
+                     </ul>
+                  </div>
+             </div>                       
+          </p>
+         </div>
     </div>
+ </div>
   )
 }
 
